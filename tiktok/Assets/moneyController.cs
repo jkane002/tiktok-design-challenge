393cb53f0ac2042ae7f4858c6ec8f22e374f1dca
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2019-06-23 14:34:23 -0700

    early prototype

---
+++ tiktok/Assets/moneyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class moneyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject HarrietTubmanBill;
+    bool ifPressed = false;
+    bool isTubmanActive = false;
+    
+    void Start()
+    {
+        HarrietTubmanBill.SetActive(isTubmanActive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ifPressed = CrossPlatformInputManager.GetButton("SwitchButton");
+        if(ifPressed) {
+            HarrietFn();
+        }
+    }
+
+    void HarrietFn() {
+        if (isTubmanActive)
+        {
+            isTubmanActive = false;
+            HarrietTubmanBill.SetActive(isTubmanActive);
+        }
+        else if (!isTubmanActive)
+        {
+            isTubmanActive = true;
+            HarrietTubmanBill.SetActive(isTubmanActive);
+        }
+    }
+}
+
+
